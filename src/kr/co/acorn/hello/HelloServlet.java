eda2f283de5b6b1716c9403fa1d3fe7da77bd571
Gahee707l <gahee707l@gmail.com>
2019-12-17 14:49:53 +0900

    20191217.v1

---
+++ src/kr/co/acorn/hello/HelloServlet.java
@@ -0,0 +1,52 @@
+package kr.co.acorn.hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response MIME time과 문자 인코딩 지정
+		response.setContentType("text/html;charset=utf-8");
+		//클라이언트에게 응답할 출격 객체 생성
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello ㅅㅇㅎ!</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		//print로 하면 일렬로 주욱 나옴
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
